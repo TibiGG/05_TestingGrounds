33ed49aedeb2c29b06085fdadd8ea4ef689f0698
Lenovo\Computer <tiberiu_andrei99@yahoo.com>
2017-08-19 17:08:52 +0300

    C++ AI Behaviour Tree Tasks

---
+++ Source/S05_TestingGrounds/Private/ChooseNextWaypoint.cpp
@@ -0,0 +1,11 @@
+// Copyright SeeShore Games.inc
+
+#include "ChooseNextWaypoint.h"
+
+EBTNodeResult::Type UChooseNextWaypoint::ExecuteTask(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory)
+{
+	UE_LOG(LogTemp, Warning, TEXT("AI in C++ - yay!"));
+	return EBTNodeResult::Succeeded;
+}
+
+
